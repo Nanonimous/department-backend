f02b2c8e363fb7d0e13e1583c23674440397d235
gowthamhirthick@gmail.com <gowthamhirthick@gmail.com>
2025-02-03 15:49:30 +0530

    hirthick event edit button backend

--- routes/Event_page/Major_Event_backend.js
+++ routes/Event_page/Major_Event_backend.js
@@ -7,7 +7,7 @@ const route = Router()
 let collection;
 const upload = multer().single('file')
 
-
+// Major events Get request for show the content.
 route.use((req,res,next)=>{
 
     collection =  db.collection("major_events");
@@ -24,6 +24,8 @@ route.get("/",async(req,res)=>{
 
 
 
+// Major events POST request for add new content
+
 route.post("/",upload,async(req,res)=>{
 
     
@@ -38,5 +40,44 @@ route.post("/",upload,async(req,res)=>{
 
     res.json({message:"request came in post major event"})
 })
+
+// Major events DELETE request to delete the journal
+
+route.delete("/",async(req,res)=>{
+    
+    const ob =  req.body.object
+    const objectId = new ObjectId(ob);
+    console.log(ob)
+    const result = await collection.deleteOne({ _id: objectId });
+
+
+    res.json({message:"deleted successfully"})
+})
+
+
+// Major events  PUT request to edit the content
+
+route.put("/",upload,async(req,res)=>{
+
+     
+    const file = req.file
+    const data = JSON.parse(req.body.data)
+    
+    const filter = { _id : new ObjectId(data.videoId) };
+    delete data.videoId
+
+    
+    if (file == null){
+        const result = await collection.updateOne(filter,{$set:{data}})
+    }else{
+        const result = await collection.updateOne(filter,{$set:{file,data}})
+    }
+
+   
+
+    res.json({message:"success"})
+
+})
+
 
 export default route
